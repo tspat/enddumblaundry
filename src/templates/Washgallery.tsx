b5ba5de783bea9d73b65a2dc9d6211ee82055c4d
tspat <63246265+tspat@users.noreply.github.com>
2022-06-23 09:55:27 -0700

    app and dots

--- src/templates/Washgallery.tsx
+++ src/templates/Washgallery.tsx
@@ -31,7 +31,7 @@ export default function Washgallery() {
                           height={20}
                           patternUnits="userSpaceOnUse"
                         >
-                          <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                          {/* <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" /> */}
                         </pattern>
                       </defs>
                       {/* <rect width={784} height={404} fill="url(#ca9667ae-9f92-4be7-abcb-9e3d727f2941)" /> */}
@@ -60,7 +60,7 @@ export default function Washgallery() {
                           height={20}
                           patternUnits="userSpaceOnUse"
                         >
-                          <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                          {/* <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" /> */}
                         </pattern>
                       </defs>
                       {/* <rect width={784} height={404} fill="url(#ca9667ae-9f92-4be7-abcb-9e3d727f2941)" /> */}
@@ -94,7 +94,7 @@ export default function Washgallery() {
                       <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
                     </pattern>
                   </defs>
-                  <rect width={404} height={784} fill="url(#b1e6e422-73f8-40a6-b5d9-c8586e37e0e7)" />
+                  {/* <rect width={404} height={784} fill="url(#b1e6e422-73f8-40a6-b5d9-c8586e37e0e7)" /> */}
                 </svg>
 
                 <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
@@ -124,7 +124,7 @@ export default function Washgallery() {
                           height={20}
                           patternUnits="userSpaceOnUse"
                         >
-                          <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                          {/* <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" /> */}
                         </pattern>
                       </defs>
                       <rect width={784} height={404} fill="url(#ca9667ae-9f92-4be7-abcb-9e3d727f2941)" />
@@ -158,7 +158,7 @@ export default function Washgallery() {
                       <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
                     </pattern>
                   </defs>
-                  <rect width={404} height={784} fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)" />
+                  {/* <rect width={404} height={784} fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)" /> */}
                 </svg>
 
                 <div className="relative mt-12 sm:mt-16 lg:mt-24">
@@ -188,7 +188,7 @@ export default function Washgallery() {
                             height={20}
                             patternUnits="userSpaceOnUse"
                           >
-                            <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                            {/* <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" /> */}
                           </pattern>
                         </defs>
                         <rect width={784} height={404} fill="url(#e80155a9-dfde-425a-b5ea-1f6fadd20131)" />
@@ -229,7 +229,7 @@ export default function Washgallery() {
                           height={20}
                           patternUnits="userSpaceOnUse"
                         >
-                          <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                          {/* <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" /> */}
                         </pattern>
                       </defs>
                       <rect width={784} height={404} fill="url(#ca9667ae-9f92-4be7-abcb-9e3d727f2941)" />
