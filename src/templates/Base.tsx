c327c7696a723f5439267b15ba138704b89ae0d4
tspat <63246265+tspat@users.noreply.github.com>
2022-06-27 12:49:43 -0700

    linkedinpixel

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -40,6 +40,23 @@ const Base = () => (
             twq('track','PageView');
                 `}
       </Script>
+      <Script>{`<script type="text/javascript">
+        _linkedin_partner_id = "1445460";
+        window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+        window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+        </script><script type="text/javascript">
+        (function(l) {
+        if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+        window.lintrk.q=[]}
+        var s = document.getElementsByTagName("script")[0];
+        var b = document.createElement("script");
+        b.type = "text/javascript";b.async = true;
+        b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+        s.parentNode.insertBefore(b, s);})(window.lintrk);
+        </script>
+        <noscript>
+        <img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=1445460&fmt=gif" />
+        </noscript>`}</Script>
       <Script async defer src="//js.hs-scripts.com/9146160.js"></Script>
       <Script id="google-tag-manager" strategy="afterInteractive" async src="https://www.googletagmanager.com/gtag/js?id=G-HK6EEB64WX"></Script>
       <Script id="google-tag-manager" strategy="afterInteractive">
